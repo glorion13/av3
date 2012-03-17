2983580e51b9f16a409474d01c12fbf5306de380
Boris Mitrovic <boris.mitrovic@gmail.com>
2012-03-17 16:22:55 +0000

    Add initial plane points

--- main.m
+++ main.m
@@ -3,6 +3,9 @@
 % Initialize data locations
 imagesDir = 'data/bindermat';
 backgroundDir = 'data/field.jpg';
+initialPointsXY = [87,176; 90,371;402,144;407,400]; % image (x, y)
+
+
 
 % Main loop
 
@@ -12,6 +15,7 @@ backgroundImage = imread(backgroundDir);
 
 % Transforming images so that they are displayed correctly
 transformedImages = transformData(images);
+initialPoints = getRange(transformedImages(:,:,:,2), initialPointsXY);
 
 % Overlaying background image on the actual video
 remapped = remap(backgroundImage, transformedImages(:,:,:,3));
