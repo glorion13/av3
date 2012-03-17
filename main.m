f6a01df617680dabc8df4eff63ba489c891a3485
Alexandros Gouvatsos <alex____13@hotmail.com>
2012-03-17 12:52:21 +0000

    updated main.m

--- main.m
+++ main.m
@@ -1,4 +1,13 @@
 % Main
 
-images = readData('data/bindermat');
+% Initialize data locations
+
+imagesDir = 'data/bindermat';
+backgroundDir = 'data/field.jpg';
+
+% Main loop
+
+images = readData(imagesDir);
 transformedImages = transformData(images);
+
+backgroundImage = imread(backgroundDir);
