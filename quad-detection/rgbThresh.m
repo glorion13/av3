06d0294a11a366468392e8f707ad067e1a6fc81b
Alexandros Gouvatsos <alex____13@hotmail.com>
2012-03-18 12:16:52 +0000

    cleaned up rgbThresh

--- quad-detection/rgbThresh.m
+++ quad-detection/rgbThresh.m
@@ -1,4 +1,4 @@
-function outImage = rgbThresh(inImage)
+function indeces = rgbThresh(inImage)
     
     rgbImage = inImage;
 
@@ -11,19 +11,11 @@ function outImage = rgbThresh(inImage)
     % Get the blue plane
     bPlane = rgbImage(:,:,3);
     
-    % Find indeces that will be removed based on thresholding
+    % Find indeces that will be returned based on thresholding
     rThresh = (rPlane<0.176)&(rPlane>0.047);
     gThresh = (gPlane<0.09)&(gPlane>0.027);
     bThresh = (bPlane<0.15)&(bPlane>0.007);
     
-    indeces = find((rThresh&gThresh&bThresh));
-    
-    outImage = rgbImage;
-    
-    % Remove selected pixels
-    outImage(indeces) = 255;
-    %outImage(~indeces) = 1;
-    figure(1);
-    imshow(outImage);
+    indeces = (rThresh&gThresh&bThresh);
     
 end
