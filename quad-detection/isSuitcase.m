bde3528c070d0a6fb336dd48c0d3ebd76f03a479
Boris Mitrovic <boris.mitrovic@gmail.com>
2012-04-21 22:52:07 +0100

    add a comment

--- quad-detection/isSuitcase.m
+++ quad-detection/isSuitcase.m
@@ -2,6 +2,7 @@ function [ isSuitcase ] = isSuitcase( pts3d )
     % This function simply checks if a set of points in 3D space is large
     % enough to be assumed to be the correct quad (suitcase of the man).
     isSuitcase = length(pts3d) > 5000;
+    % residual error of the plane is already checked
 
 end
 
