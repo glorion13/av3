4baede22f54654c7b72140bc2716165e42702c9f
Boris Mitrovic <boris.mitrovic@gmail.com>
2012-04-22 18:18:04 +0100

    comment main

--- main.m
+++ main.m
@@ -7,14 +7,6 @@ videoDir = 'data/video';
 
 % manually selected corners of the overlaying background image
 backgroundCorners=[[40,182]',[39,429]',[474,453]',[474,155]']';
-sizePatch= 100;
-middlePoint = round(mean(backgroundCorners));
-
-xRange = [middlePoint(1)-sizePatch, middlePoint(1)+sizePatch];
-yRange = [middlePoint(2)-sizePatch, middlePoint(2)+sizePatch];
-
-xSize = xRange(2) - xRange(1) + 1;
-ySize = yRange(2) - yRange(1) + 1;
 
 % Loading data
 images = readData(imagesDir);
@@ -28,61 +20,66 @@ earlyFrame = images(:,:,:,2);
 
 writeImgs(images,1:nImages, 'original');
 
+% Find a big patch for estimating the plane based on the four corners
+sizePatch= 100;
+middlePoint = round(mean(backgroundCorners));
+xRange = [middlePoint(1)-sizePatch, middlePoint(1)+sizePatch];
+yRange = [middlePoint(2)-sizePatch, middlePoint(2)+sizePatch];
+xSize = xRange(2) - xRange(1) + 1;
+ySize = yRange(2) - yRange(1) + 1;
+
 % Compute Background overlaying plane
 initialPoints = earlyFrame(xRange(1):xRange(2),yRange(1):yRange(2),1:3);
 initialPoints = reshape(initialPoints(:,:,1:3), xSize*ySize, 3);
 
 [plane, fit] = fitplane(initialPoints);
 
-%P = reshape(earlyFrame(:,:,1:3), 480*640, 3);
-%[newlist, remaining] = getallpoints(plane, initialPoints, P, length(P));
-% [plane, fit] = fitplane(newlist);
 
-% Overlaying background image on the actual video
+% Iterate over the images
 for i=1: nImages
     
     frame = i
-    
-    % Working on quad
-    %if i > 13 && i < 29
         
-        for j=1:2
-            attempt = j
-            [ quadPoints, suitcasePlane ] = planeExtraction(images(:,:,:,i));
-            if isSuitcase(quadPoints)
-                break;
-            end
-        end
-        
+    % In case of a very improbable event of not finding the best fitting
+    % plane, while there is one, run the algorithm again
+    for j=1:2      
+        attempt = j
+        [ quadPoints, suitcasePlane ] = planeExtraction(images(:,:,:,i));
         if isSuitcase(quadPoints)
-            
-            % find ordered corners of the suitcase
-            suitcaseCorners = getCorners(quadPoints);
-            pixelVals = getPixelVals(suitcaseCorners, images(:,:,:,i));
-            orderedCorners = orderCorners(pixelVals);
-            
-            % Load corresponding video frame
-            listing = dir( videoDir );
-            imagePath = strcat(videoDir, '/', listing( i+3 ).name);
-            videoFrame = double(imread(imagePath));
+            break;
+        end
+    end
 
-            % remap on suitcase
-            images(:,:,:,i) = remap(videoFrame, images(:,:,:,i), suitcasePlane, orderedCorners, 0.02);
-            writeImgs(images,i, 'quad');
-        end            
-    %end
+    if isSuitcase(quadPoints)
+        % find ordered corners of the suitcase
+        suitcaseCorners = getCorners(quadPoints);
+        pixelVals = getPixelVals(suitcaseCorners, images(:,:,:,i));
+        orderedCorners = orderCorners(pixelVals);
+
+        % Load corresponding video frame
+        listing = dir( videoDir );
+        imagePath = strcat(videoDir, '/', listing( i+3 ).name);
+        videoFrame = double(imread(imagePath));
+
+        % Remap video on the suitcase
+        images(:,:,:,i) = remap(videoFrame, images(:,:,:,i), suitcasePlane, orderedCorners, 0.02);
+    end
+
+    writeImgs(images,i, 'quad');
     
     % Make pixels without any value into background pixels
     images(:,:,:,i) = fillMissingVals(images(:,:,:,i), earlyFrame(:,:,4:6));
     writeImgs(images,i, 'fill');   
+    
     % Remap field image as background image
     UV=backgroundCorners;    % target points
     images(:,:,:,i) = remap(backgroundImage, images(:,:,:,i), plane, UV, 0.05);
     
-    figure(3)
     imshow(images(:,:,4:6,i));
 end
 
 % create the AV.avi video of all the frames
 generateVideo(images);
+
+% write all the final images to the output folder
 writeImgs(images,1:nImages, 'final');
